a0a7407cdc518a0e4e1ddad2db910abbe96339b0
Sichun Qin <sichunqin@hotmail.com>
2024-07-11 15:14:34 +0800

    modify gpio port number to 512 to work on x86 enviroment

--- 08_timer/mytimer.c
+++ 08_timer/mytimer.c
@@ -9,11 +9,14 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Johannes 4 GNU/Linux");
 MODULE_DESCRIPTION("A simple test for LKM's timer");
 
+#define MY_GPIO_PIN 512
+
 /** variable for timer */
 static struct timer_list my_timer;
 
 void timer_callback(struct timer_list * data) {
-	gpio_set_value(4, 0); /* Turn LED off */
+	gpio_set_value(MY_GPIO_PIN, 0); /* Turn LED off */
+	printk("set GPIO%d: to 0\n",MY_GPIO_PIN);
 }
 
 /**
@@ -22,20 +25,32 @@ void timer_callback(struct timer_list * data) {
 static int __init ModuleInit(void) {
 	printk("Hello, Kernel!\n");
 	/* GPIO 4 init */
-	if(gpio_request(4, "rpi-gpio-4")) {
-		printk("Can not allocate GPIO 4\n");
-		return -1;
-	}
+	int ret =  gpio_request(MY_GPIO_PIN, "rpi-gpio-4");
+	
+        if (ret) {
+     	   if (ret == -EBUSY) {
+        	    pr_err("GPIO %d is already in use (Error: %d)\n", MY_GPIO_PIN, ret);
+	    } else if (ret == -EINVAL) {
+        	 pr_err("Invalid GPIO %d (Error: %d)\n", MY_GPIO_PIN, ret);
+	    } else if (ret == -EPERM) {
+           	 pr_err("Permission denied for GPIO %d (Error: %d)\n", MY_GPIO_PIN, ret);
+	    } else if (ret == -ENOSYS) {
+            	pr_err("GPIO subsystem not supported (Error: %d)\n", ret);
+	    } else {
+            	pr_err("gpio_request failed for GPIO %d with error %d\n", MY_GPIO_PIN, ret);
+       	    }
+       	    return ret;
+  	 }
 
 	/* Set GPIO 4 direction */
-	if(gpio_direction_output(4, 0)) {
+	if(gpio_direction_output(MY_GPIO_PIN, 0)) {
 		printk("Can not set GPIO 4 to output!\n");
 		gpio_free(4);
 		return -1;
 	}
 
 	/* Turn LED on */
-	gpio_set_value(4, 1);
+	gpio_set_value(MY_GPIO_PIN, 1);
 
 	/* Initialize timer */
 	timer_setup(&my_timer, timer_callback, 0);
@@ -50,7 +65,7 @@ static int __init ModuleInit(void) {
  */
 static void __exit ModuleExit(void) {
 	printk("Goodbye, Kernel\n");
-	gpio_free(4);
+	gpio_free(MY_GPIO_PIN);
 	del_timer(&my_timer);
 }
 
